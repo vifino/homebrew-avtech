6d19c4ba54bcb5b73908a01aca3873a0ba1abb59
Adrian Pistol <vifino@posteo.net>
2025-05-05 18:40:16 +0200

    feat(casparcg-client): init at 2.3.0

---
+++ Casks/casparcg-client.rb
@@ -0,0 +1,20 @@
+cask "casparcg-client" do
+  version "2.3.0"
+  sha256 arm:   "59fa05f530e544b1aea9b0134babefe0dceb1a85c22511dafc45aed4fb0ad218",
+         intel: "c3e04612061ba9d13b199e82cc24f5393adced18fe885e69730b390ecbe081bc"
+
+  on_arm do
+    url "https://github.com/CasparCG/client/releases/download/v#{version}/casparcg-client-v#{version}-macos-arm64.dmg"
+  end
+  on_intel do
+    url "https://github.com/CasparCG/client/releases/download/v#{version}/casparcg-client-v#{version}-macos-x86_64.dmg"
+  end
+
+  name "CasparCG Client"
+  desc "Client for controlling the CasparCG media server"
+  homepage "https://github.com/CasparCG/client"
+
+  app "CasparCG Client.app"
+
+  zap trash: "~/.CasparCG/"
+end
